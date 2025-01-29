f18230fadfbfa37742600b17c00a7e407c0dea05
Saif Raihan <saifraihan@Saifs-MacBook-Pro.local>
2025-01-29 14:54:05 -0800

    02

--- 02-can-you-write-a-function-in-javascript-to-remove-duplicate-elements-from-an-array?/solution.js
+++ 02-can-you-write-a-function-in-javascript-to-remove-duplicate-elements-from-an-array?/solution.js
@@ -1,7 +1,11 @@
 // Solution for: Can you write a function in JavaScript to remove duplicate elements from an array?
 
-function solution() {
-  // Write your solution here
+function solution(arr) {
+  const newArr = new Set(arr);
+  return Array.from(newArr)
 }
 
+
 module.exports = solution;
+const arr = [1, 2, 2, 3, 4, 4, 5]
+console.log(solution(arr));
